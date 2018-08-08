6d625a08ef3f94d850cc8486d90d403ffcb35ece
mag-chang <nkhrk.dev@gmail.com>
2018-08-08 17:09:12 +0900

    create forms for customer

--- customers/urls.py
+++ customers/urls.py
@@ -4,5 +4,7 @@ from customers import views
 app_name = 'customers'
 urlpatterns = [
     # 顧客
-    path('list/', views.get_customer_list),   # 一覧
+    path('list/', views.get_customer_list, name='list'),   # 一覧
+    path('add/', views.customer_edit, name='add'),  # 登録
+    path('edit/<int:customer_id>/', views.customer_edit, name='edit'),  # 修正
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from customers.models import Customer
+from customers.forms import CustomerForm
+
 
 def get_customer_list(request):
     """
@@ -11,11 +13,39 @@ def get_customer_list(request):
     """
 
     customers = Customer.objects.all().order_by('id')
-    # return render(request,
-    #               'customers/list.html',
-    #               {'customers': customers},
-    #               )
     return render(request,
                   'customers/list.html',
-                  {'customers': '田中太郎'},
+                  {'customers': customers},
                   )
+    # return render(request,
+    #               'customers/list.html',
+    #               {'customers': '田中太郎'},
+    #               )
+
+def customer_edit(request, customer_id=None):
+    """
+    顧客を新規作成/編集する
+    :param request:
+    :param customer_id:
+    :return:
+    """
+
+    if customer_id:
+        customer = get_object_or_404(Customer, pk=customer_id)
+    else:
+        customer = Customer()
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, instance=customer)
+        if form.is_valid():
+            customer = form.save(commit=False)
+            customer.save()
+            return redirect('customers:list')
+    else:
+        form = CustomerForm(instance=customer)
+
+    return render(
+        request,
+        'customer/edit.html',
+        dict(form=form, customer_id=customer_id)
+    )
